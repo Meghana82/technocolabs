d31e0357097b71eecc8ec31ab406cfc22127488b
Meghana82 <63181485+Meghana82@users.noreply.github.com>
2021-06-19 11:19:02 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const kelvin = 293;
-const celsius = kelvin-273;
-let fahrenheit =  celsius * (9/5) + 32;
-//  given equation
-fahrenheit = Math.floor(fahrenheit);
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
-//console
